891d8430bf537e01c2bab237656e6458a033bf25
evathinking007 <157212190+evathinking007@users.noreply.github.com>
2024-01-29 01:24:38 +0800

    Update predit.py

--- predit.py
+++ predit.py
@@ -3,9 +3,10 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-
+import random
+import matplotlib.pyplot as plt
 # 加载数据
-file_path = 'bitcoin_data_with_indicators2.xlsx'  # 更改为您的文件路径
+file_path = 'bitcoin_data_with_indicators.xlsx'  # 更改为您的文件路径
 bitcoin_data = pd.read_excel(file_path)
 
 # 选择特征和目标变量
@@ -32,17 +33,36 @@ print(f"决定系数 (R^2): {r2}")
 
 # 使用模型进行未来价格预测
 # 提取最后一个时间点的特征数据
-last_features = bitcoin_data[[ 'Volume','MA']].iloc[0].values
+
+
 
 
 
 # 预测未来10个价格值
-future_prices = []
-for _ in range(1):
-    future_price = model.predict([last_features])[0]
-    future_prices.append(future_price)
-    # 更新特征数据，将新的预测值添加到特征数据的末尾
-    last_features = np.append(last_features[1:], [future_price])
+deviation_rates = []
+for _ in range(50):
+    random_integer = random.randint(1, 1000)
+    last_feature = bitcoin_data[[ 'Volume','MA']].iloc[random_integer].values
+    current_price=bitcoin_data[['Price']].iloc[random_integer-1].values
+    future_price = model.predict([last_feature]) 
+    deviation_rate = abs(((current_price - future_price) / future_price) * 100)
+    deviation_rates.append(deviation_rate[0])
+    print("分析时间"+str(bitcoin_data[['Date']].iloc[random_integer].values))  
+    print("未来价格预测:"+str(future_price))
+    print("真实下一价格:"+str(current_price))
+    print(f"偏差率为: {deviation_rate}%")
+   
 
-print("未来1个价格预测:")
-print(future_prices)
+print(f"偏差率中的最大值是: {max(deviation_rates)}")
+print(f"偏差率中的最小值是: {min(deviation_rates)}")
+print(f"偏差率中的平均值是: {sum(deviation_rates) / len(deviation_rates)}")
+# count_near_zero = sum(1 for value in deviation_rates if abs(value) < 0.05)
+# percentage_near_zero = (count_near_zero / len(deviation_rates)) * 100
+# print(f"接近0的数值的比例为: {percentage_near_zero}%")
+# print(deviation_rates)
+plt.hist(deviation_rates, bins=20, color='blue', alpha=0.7)
+plt.xlabel('数值')
+plt.ylabel('频率')
+plt.title('数据的直方图')
+plt.grid(True)
+plt.show()
